3495b9305c47d21af0a62fbfe8742f21236b9e10
Kam <Kam.@reedonline.co.uk>
2020-11-08 18:27:30 +0000

    test for - WHEN filtering by broadband THEN show the 4 broadband only deals

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -14,4 +14,16 @@ describe("filter", () => {
     expect(result).toEqual(mockData.deals);
     expect(result.length).toEqual(11);
   });
+
+  it('should return 4 broadband deals when filtered by broadband', () => {
+    // Arrange
+    const sut = new Store();
+    sut.setDeals(mockData.deals);
+
+    // Act
+    sut.setProductFilter('broadband');
+
+    // Assert
+    expect(sut.deals.length).toEqual(4);
+  });
 });
